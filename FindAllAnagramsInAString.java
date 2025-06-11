6f68bdef31effd1e8241ec39fb7a50ddb0a47397
sriram036 <sriramravichandran2608@gmail.com>
2025-06-11 11:48:02 -0700

    Find All Anagrams In a String

--- FindAllAnagramsInAString.java
+++ FindAllAnagramsInAString.java
@@ -5,8 +5,30 @@ public class FindAllAnagramsInAString{
     public static void main(String[] args){
         String s = "aaabb";
         String p = "bb";
-        List<Integer> list = findAnagrams(s, p);
+        List<Integer> list = findAnagrams2(s, p);
         System.out.println(list);
+    }
+    public static List<Integer> findAnagrams2(String s, String p) {
+        int ns = s.length(), np = p.length();
+        if (ns < np) return new ArrayList<>();
+
+        int [] pCount = new int[26];
+        int [] sCount = new int[26];
+        for (char ch : p.toCharArray()) {
+            pCount[(int)(ch - 'a')]++;
+        }
+
+        List<Integer> output = new ArrayList<>();
+        for (int i = 0; i < ns; ++i) {
+            sCount[(int)(s.charAt(i) - 'a')]++;
+            if (i >= np) {
+                sCount[(int)(s.charAt(i - np) - 'a')]--;
+            }
+            if (Arrays.equals(pCount, sCount)) {
+                output.add(i - np + 1);
+            }
+        }
+        return output;
     }
     public static List<Integer> findAnagrams(String s, String p) {
         List<Integer> list = new ArrayList<Integer>();
